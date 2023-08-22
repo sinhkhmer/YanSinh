f6685d66b0c92e7c227903f6d8615808e2109020
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-08-22 21:40:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-if (username === "sinh@123" && password === "sinh@123") {
-    console.log("Login successful. Redirecting...");
-    window.location.href = "welcome.html";
-} else {
-    console.log("Invalid credentials. Please try again.");
-    alert("Invalid credentials. Please try again.");
-}
+
+// script.js
+const loginForm = document.getElementById('login-form');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    // Perform your login validation here
+    if (username === 'sinh@123' && password === 'sinh@123') {
+        alert('Login successful');
+    } else {
+        alert('Invalid credentials. Please try again.');
+    }
+});
