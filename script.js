024bd3519e9a2bff39abad0f395ba2120d95ad93
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-09-09 20:29:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-// script.js
-const loginForm = document.getElementById('login-form');
+        const loginForm = document.getElementById('login-form');
+        const feedback = document.getElementById('feedback');
 
-loginForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-    // Perform your login validation here
-    if (username === 'sinh@123' && password === 'aR4@fgd#shdj18dET26') {
-        // Redirect to the welcome page on successful login
-        window.location.href = 'welcome.html';
-    } else {
-        alert('លិខិត​សម្គាល់​មិន​ត្រឹមត្រូវ។ សូម​ព្យាយាម​ម្តង​ទៀត។');
-    }
-});
+        loginForm.addEventListener('submit', function(event) {
+            event.preventDefault();
 
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+
+            if (username === 'sinh@123' && password === 'sinh@123') {
+                feedback.textContent = '';
+                window.location.href = 'welcome.html';
+            } else {
+                feedback.textContent = 'លិខិត​សម្គាល់​មិន​ត្រឹមត្រូវ។ សូម​ព្យាយាម​ម្តង​ទៀត។';
+            }
+        });
+
+        const togglePasswordButton = document.getElementById('toggle-password');
+        const passwordInput = document.getElementById('password');
+        
+        togglePasswordButton.addEventListener('click', function() {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+            } else {
+                passwordInput.type = 'password';
+            }
+        });
