70e5ae0cb681de569d14bb2af1661925461eed59
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-10-01 20:32:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,19 @@
                 passwordInput.type = 'password';
             }
         });
+document.addEventListener('DOMContentLoaded', function() {
+    const backgroundMusic = document.getElementById('backgroundMusic');
+
+    // Automatically play the background music
+    backgroundMusic.play();
+
+    // Set the volume to 100%
+    backgroundMusic.volume = 0.5; // 1.0 represents 100% volume
+
+    // Set up event listener to replay when it ends
+    backgroundMusic.addEventListener('ended', function() {
+        backgroundMusic.currentTime = 0; // Reset the playback to the beginning
+        backgroundMusic.play();
+    });
+});
+
