d58aa72884e92e91a156feb1f975189a9419255a
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-10-11 19:11:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
         const loginForm = document.getElementById('login-form');
         const feedback = document.getElementById('feedback');
-
         loginForm.addEventListener('submit', function(event) {
             event.preventDefault();
-
             const username = document.getElementById('username').value;
             const password = document.getElementById('password').value;
-
             if (username === 'sinh@123' && password === 'sinh@123') {
                 feedback.textContent = '';
                 window.location.href = 'welcome.html';
@@ -14,10 +11,8 @@
                 feedback.textContent = 'លិខិត​សម្គាល់​មិន​ត្រឹមត្រូវ។';
             }
         });
-
         const togglePasswordButton = document.getElementById('toggle-password');
         const passwordInput = document.getElementById('password');
-        
         togglePasswordButton.addEventListener('click', function() {
             if (passwordInput.type === 'password') {
                 passwordInput.type = 'text';
@@ -26,8 +21,11 @@
             }
         });
 document.addEventListener('DOMContentLoaded', function() {
-    const backgroundMusic = document.getElementById('backgroundMusic');
-    backgroundMusic.volume = 0.5;
+    var audio = document.getElementById('backgroundMusic');
+    audio.volume = 1.0; 
+    audio.muted = false;
+    audio.play();
 });
+
 
 
